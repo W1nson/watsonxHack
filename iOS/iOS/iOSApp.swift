e6fcb5d1d7793fa7c751b4fe2cc84cf0857cc7a9
jessicatrans <jesstrans02@gmail.com>
2025-08-03 23:19:10 -0700

    update swift

---
+++ iOS/iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Jessica Trans on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
